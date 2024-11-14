7f255789769f9e6cb48064aa46eae547a8998e9f
xFelixDev <felixpapiernik42@gmail.com>
2024-11-14 17:24:35 +0100

    Project init

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <>
+            <h3>Base page</h3>
+            <Link href="/login">Login</Link>
+        </>
+    );
+}
