dc636ec53c186728d3c1ab5eacabac83f38dedd2
xFelixDev <felixpapiernik42@gmail.com>
2025-01-30 16:07:25 +0100

    loading ui

--- src/app/u/moje-knihy/page.tsx
+++ src/app/u/moje-knihy/page.tsx
@@ -12,10 +12,12 @@ export default function MojeKnihy() {
 
     const { auth } = useAuth();
     const [userBooks, setUserBooks] = useState([] as EntityGroupedData[]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         if (!auth) return;
         const getUserBooks = async () => {
+            setLoading(true);
             return await getPouzivateloveKnihy(auth?.pouzivatel.id)
         }
 
@@ -26,6 +28,7 @@ export default function MojeKnihy() {
                 view: { detailUrl: `/u/moje-knihy/${k.id}` }
             })) as EntityGroupedData[]
             setUserBooks(knihaGroupedData);
+            setLoading(false);
         });
 
         const cleanUp = () => {
@@ -38,10 +41,12 @@ export default function MojeKnihy() {
         <>
             <Typography variant="h3" mt={4} mb={2}>Moje knihy</Typography>
             <Typography variant="body1">Tuto nájdeš knihy, ktoré si si uložil do zoznamu, alebo si ich už prečítal.</Typography>
+            {loading === false ? 
             <EntityList
                 data={userBooks}
                 notFoundElement={<Typography>V tvojom zozname nie sú žiadne knihy, <Link href={"/knihy"}>poď si ich nájsť</Link></Typography>}
             />
+            : <Typography variant="h6">Načítavam tvoje knihy...</Typography>}
         </>
     )
 }
